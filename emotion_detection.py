9c66c21a4fbc8807210a4d41ad0b187804aa7685
AmanyaPhillip <amanyaphillip@outlook.com>
2025-06-26 15:26:18 -0400

    Editted emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -14,4 +14,36 @@ def emotion_detection(text_to_analyze):
     #Make the Post request to the API with the payload and header
     response = requests.post(url, json = myobj , headers = header)
 
-    return response.text
+    #parse the response from the API
+    formatted_response = json.loads(response.text)
+
+    # If the response status code is 200, extract the label and score from the response
+    if response.status_code == 200:
+        anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    # If the response status code is 500, set label and score to None
+    elif response.status_code == 500:
+        anger = None
+        disgust = None
+        fear = None
+        joy = None
+        sadness = None
+
+    data = {
+        "anger": anger,
+        "disgust":disgust,
+        "fear":fear,
+        "joy":joy,
+        "sadness":sadness
+    }
+    largest_value = 0
+    for name, value in data.items():
+        if value > largest_value:
+            largest_value = value
+            largest_variable_name = name
+    
+    data['dominant_emotion'] = largest_variable_name
+    return data
